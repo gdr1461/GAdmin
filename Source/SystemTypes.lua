8170dbff6e4c5f453bf153cf8829b237120baac5
gdr <114087549+gdr1461@users.noreply.github.com>
2024-08-30 14:15:38 +0700

    Settings update
    v1.2.1

--- Source/SystemTypes.lua
+++ Source/SystemTypes.lua
@@ -1,296 +1,89 @@
---________________________________________________________________________________________________________________________________________________________
+local Settings = {}
 
-
---====== SystemTypes is every type of GAdmin's module.
---=== You'll need them for autofill in your addons.
-
---________________________________________________________________________________________________________________________________________________________
-
--- << OTHER >>
-
-local TopBarPlusTypes = require(script.TopBarPlus)
-export type UserId = {}
-export type Banned = {
-	Reason: string,
-	Time: string
-}
-
-export type DefaultPlayerData = {
-	Rank: string,
-	ServerRank: number,
-	RankExpiresIn: any,
-	Prefix: string,
-	DefaultKickMessage: string,
-	DefaultBanMessage: string,
-	Banned: false | Banned,
-}
-
-export type ArgumentsSigns = {
-	Optional: string,
-	MultiType: string,
-	InGame: string,
-	OptionalInGame: string,
-	FilterString: string,
-	EqualRank: string,
-}
-
-export type RankType = {Rank: number, Users: {number | string}}
-export type CollisionGroups = "Default" | "GAdmin Players" | "GAdmin NonPlayers" | "GAdmin NonCollide"
-
-export type MessageTopic = {
-	Topic: string,
-	Arguments: {any}
-}
-
-export type WindowControls = {
-	Frame: Frame,
-	Remove: () -> (),
-	ChangeState: (Boolean: boolean?) -> (),
-	GetState: () -> boolean,
-	NextMovement: (Keys: {Enum.KeyCode}, Speed: number, DeltaTime: number) -> (CFrame, number),
-	ClickButton: () -> (),
-}
-
-export type InputWindowControls = {
-	Frame: Frame,
-	Remove: () -> (),
-	NextMovement: (Keys: {Enum.KeyCode}, Speed: number, DeltaTime: number) -> (CFrame, number),
-	ClickButton: () -> (),
-}
-
-export type MarkdownData = {
-	Button: TextButton,
-	State: boolean,
-	ChangeState: (self: MarkdownData, Boolean: boolean) -> ()
-}
-
-export type HoverData = {
-	Gui: ScreenGui,
-	Info: string,
-	Object: GuiObject,
-}
-
-export type WindowData = {
-	Size: "Small" | "Normal" | "Big" | "Giant",
-
-	Inputs: {
-		{
-			DefaultInput: string?,
-			Name: string,
-			OnChange: (WindowControls: WindowControls, Input: string) -> ()
-		}
+--== << RANKS >> ==--
+Settings.Ranks = { -- In Users you can put either userid or username.
+	Owner = {
+		Rank = 5,
 	},
 
-	UserInputService: {(WindowControls: WindowControls, Began: boolean, InputKey: Enum.KeyCode, InputType: Enum.UserInputType) -> ()},
-	Connections: {RBXScriptConnection},
-
-	OnClicked: (WindowControls: WindowControls, State: boolean) -> (),
-	OnRemove: (WindowControls: WindowControls) -> (),
-
-	ButtonOn: string,
-	ButtonOff: string,
-}
-
-export type InputWindowData = {
-	Size: "Normal" | "Big",
-
-	Input: {
-		DefaultInput: string?,
-		OnChange: (WindowControls: InputWindowControls, Input: string) -> ()
+	ChiefAdmin = {
+		Rank = 4,
+		Users = {}
 	},
 
-	Connections: {RBXScriptConnection},
-	OnClicked: (WindowControls: InputWindowControls) -> (),
-	OnRemove: (WindowControls: InputWindowControls) -> (),
-}
-
-
---________________________________________________________________________________________________________________________________________________________
-
--- << SERVER >>
-
-export type ServerData = {
-	ClientConfigured: boolean,
-	SessionData: DefaultPlayerData,
-	
-	TempData: {
-		[UserId]: {
-			Health: number,
-			MaxHealth: number,
-			Speed: number,
-			JumpPower: number,
-		}
+	Admin = {
+		Rank = 3,
+		Users = {}
 	},
-	
-	Logs: {},
-	ChatLogs: {},
-	
-	ServerRankAccess: number,
-	Loops: {},
-	DefaultPlayerData: DefaultPlayerData,
-	EditablePlayerData: {},
-	Tools: {},
-	
-	CommandArgumentsSigns: ArgumentsSigns,
-	DefaultArguments: {},
-	ArgumentsTransform: {},
-	ClientCommandsList: {},
-	ClientFolder: Folder
-}
 
-export type ServerDataStoreLoader = {
-	Loaded: {},
-	Load: (Key: string, DataStore: string, Ordered: boolean?) -> (boolean, any),
-	Save: (Key: string, Value: any, DataStore: string, Ordered: boolean?) -> boolean,
-	
-	ListKeys: (DataStore: string) -> (boolean, any),
-	Order: (DataStore: string, Ascending: boolean, PageSize: number) -> DataStorePages
-}
-
-export type ServerSettings = {
-	Ranks: {any},
-	AdminAccess: number,
-	RankNoticeAccess: number,
-	BToolsAccess: number,
-
-	EveryoneAccess: number,
-	BanlistAccess: number,
-
-	DefaultKickMessage: string,
-	DefaultBanMessage: string,
-
-	CommandInLineDebounce: boolean,
-	DefaultPrefix: string,
-	PrivateServerOwner: number,
-	DefaultRank: number,
-	Banned: {},
-}
-
-export type ServerAPI = {
-	__metatable: string,
-	__type: string,
-	__ClientBlacklist: {string?},
-
-	GetPlayer: (self: ServerAPI, Name: string) -> Player,
-	GetPrefix: (self: ServerAPI, player: Player) -> string,
-
-	GetOwner: (self: ServerAPI) -> number,
-	GetRank: (self: ServerAPI, Data: string | number, AlwaysData: boolean?) -> RankType,
-	GetRankUsers: (self: ServerAPI, Rank: string) -> {number?}?,
-
-	CheckUserRank: (self: ServerAPI, player: Player) -> (),
-	GetUserRank: (self: ServerAPI, player: Player) -> number,
-	GetUserIdRank: (self: ServerAPI, UserId: number) -> number,
-	GetUserSettings: (self: ServerAPI, player: Player) -> {[string]: any?},
-	GetOrderedRanks: (self: ServerAPI, Ascending: boolean) -> {[number]: string}?,
-
-	GetBanlist: (self: ServerAPI) -> {number},
-	IsBanned: (self: ServerAPI, player: Player) -> boolean,
-
-	Ban: (self: ServerAPI, Caller: Player, UserId: number, Reason: string, Time: number?) -> (string, string?),
-	BanIP: (self: ServerAPI, Caller: Player, UserId: number, Reason: string, Time: number?) -> (string, string?),
-	
-	UnBan: (self: ServerAPI, UserId: number) -> (string, string?),
-	PushMessage: (self: ServerAPI, Data: MessageTopic) -> (),
-
-	GetSignals: (self: ServerAPI) -> {},
-	ClientCall: (player: Player, Action: string, Variables: any?) -> any?,
-
-	GetRanks: (self: ServerAPI) -> {[string]: RankType},
-	GetServerRanks: (self: ServerAPI) -> {[number]: number},
-	GetPlayerRanks: (self: ServerAPI) -> {[number]: {Global: number, Server: number}},
-}
-
---________________________________________________________________________________________________________________________________________________________
-
--- << SHARED >>
-
-export type SharedSignals = {
-	__metatable: string,
-	__type: string,
-	__Events: {[string]: {Signal: RemoteEvent | RemoteFunction, Action: string}},
-
-	__Side: string,
-	__OtherSide: string,
-	__Connections: {RBXScriptConnection},
-
-	Set: (self: SharedSignals, Folder: Folder) -> (),
-	Fire: (self: SharedSignals, Event: string, Variables: any) -> any,
-
-	FireAll: (self: SharedSignals, Event: string, Variables: any) -> (),
-	Connect: (self: SharedSignals, Event: string, Function: () -> ()) -> RBXScriptConnection?,
-	DisconnectAll: (self: SharedSignals) -> ()
-}
-
-export type SharedAPI = {
-	__metatable: string,
-	__type: string,
-
-	HeadShot: (self: SharedAPI, UserId: number) -> (string, boolean),
-	FindValue: (self: SharedAPI, Data: Dictionary, Value: any) -> Key,
-	SetModelCollision: (self: SharedAPI, Model: Model | Folder, CollisionGroup: CollisionGroups) -> (),
-}
-
---________________________________________________________________________________________________________________________________________________________
-
--- << CLIENT >>
-
-export type ClientData = {
-	MainFrames: {},
-	Loops: {},
-	Device: string,
-	Banlist: {},
-	ToUnBan: number,
-	
-	Ranks: {},
-	OrderedRanks: {},
-	AssetId: number,
-	
-	Rank: {
-		Name: string,
-		Id: number,
+	Mod = {
+		Rank = 2,
+		Users = {}
 	},
-	
-	Updates: {},
-	Settings: {},
+
+	Trusted = {
+		Rank = 1,
+		Users = {}
+	},
+
+	Default = {
+		Rank = 0
+	}
 }
 
-export type ClientAPI = {
-	__metatable: string,
-	__type: string,
+--== << NOTIFIES >> ==--
+Settings.NoNotifies = false -- User won't get notifies.
+Settings.NoWarns = false -- User won't get warning notifies.
+Settings.NoErrors = false -- User won't get error notifies.
+Settings.IncorrectCommandNotify = true -- Warn user about trying to call non existing command.
 
-	__WindowSizes: {
-		Small: UDim2,
-		Normal: UDim2,
-		Big: UDim2,
-		Giant: UDim2,
-	},
-	
-	__InputWindowSizes: {
-		Small: UDim2,
-		Normal: UDim2,
-	},
-	
-	GetTopBarPlus: (self: ClientAPI) -> TopBarPlusTypes.ModuleType,
-	GetTopBar: (self: ClientAPI) -> TopBarPlusTypes.IconType,
-	TopBarEnabled: (self: ClientAPI, Enabled: boolean) -> (),
-	
-	ConvertUDim: (self: ClientAPI, Mode: "Scale" | "Offset", Udim: UDim2) -> UDim2,
-	GetFrameMousePosition: (self: ClientAPI, Gui: ScreenGui, Frame: GuiObject, Offset: {X: number, Y: number}) -> UDim2,
-	
-	Notify: (self: ClientAPI, Type: "Notify" | "Error" | "Warn", Text: string, Timer: number?, OnInteract: () -> ()?) -> {any},
-	CreateHoverInfo: (self: ClientAPI, Object: GuiObject, Info: string) -> HoverData,
-	
-	GetMarkdown: (self: ClientAPI, Button: TextButton) -> MarkdownData,
-	CreateMarkdown: (self: ClientAPI, Button: TextButton, OnActivated: (State: boolean) -> (), Info: string?) -> MarkdownData,
-	
-	CreateWindow: (self: ClientAPI, Title: string, Data: WindowData) -> Frame,
-	CreateInputWindow: (self: APIModule, Title: string, Data: InputWindowData) -> Frame,
-	
-	CloseWindow: (self: APIModule, Title: string) -> (),
-	Clear: (self: ClientAPI) -> (),
+--== << ACCESS >> ==--
+Settings.AutoCompleteChatCommands = true -- Slash commands will be auto completed.
+Settings.AdminAccess = 1 -- Required rank for Gui to work for player. Make nil for free access.
+Settings.IPBanAccess = 4 -- Required  rank for IP bans.
+
+Settings.RankNoticeAccess = 1 -- Required rank for notice to appear saying player's rank. Make nil for all ranks.
+Settings.BToolsAccess = 4 -- Required rank to give yourself Building Tools.
+
+Settings.EveryoneAccess = 3 -- rank requirments to use All as a player.
+Settings.BanlistAccess = 3 -- Player access to banlist from provided rank and higher. (Leave blank for autofill.)
+
+--== << DEFAULT VALUES >> ==--
+Settings.DefaultKickMessage = "No Reason" -- The kick message if caller didn't put any reason themselfes.
+Settings.DefaultBanMessage = "No Reason" -- The ban message if caller didn't put any reason themselfes.
+
+Settings.DefaultRank = 0  -- Default rank.
+Settings.DefaultPrefix = ";"  -- Default prefix.
+
+--== << PRIVATE SERVER >> ==--
+Settings.PrivateServerOwner = 1 -- Default private server owner rank.
+Settings.PrivateServerBlacklist = {"Rank", "UnRank", "GlobalMessage", "Ban", "Ban2", "UnBan"} -- Blacklist of commands that private server owner can't do.
+
+--== << COMMANDS >> ==--
+Settings.CommandInLineDebounce = false -- Will the commands wait for current command to complete in the batch or not. (Batch example: ;rank ;cmds ;fly)
+Settings.CommandsPerMinute = 3 -- Will set commands per minute debounce. (if *Command Debounce* setting is on.)
+Settings.CommandDebounce = false -- If true, user wont able to call any other commands after *CommandPerMinute* setting limit.
+
+--== << GROUP RANKS >> ==--
+Settings.GroupRanks = { -- Ranks from groups.
+	[0] = { -- Group Id
+		-- Ranks
+		[255] = "Owner",
+		[254] = "ChiefAdmin",
+		[1] = "Vip"
+	}
 }
 
-local SystemTypes = {}
+--== << CONSTANT VALUES >> ==--
+Settings.Banned = {} -- Banlist.
 
-return SystemTypes
+Settings.DataStores = { -- DataStore names.
+	GlobalData = "GAdmin GlobalData",
+	PlayerData = "GAdmin PlayerData",
+}
+
+Settings.Topics = { -- Topics for MessagingService.
+	Global = "GAdmin Callback"
+}
+
+return Settings
